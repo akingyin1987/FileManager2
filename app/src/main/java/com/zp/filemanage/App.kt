c228dd13decb050639717853a37791ba8094d2cb
akingyin1987 <akingyin@163.com>
2019-06-06 11:38:50 +0800

    Initial commit

---
+++ app/src/main/java/com/zp/filemanage/App.kt
@@ -0,0 +1,30 @@
+package com.zp.filemanage
+
+import android.app.Application
+import com.zp.file.common.FileManageHelp
+import com.zp.file.content.*
+import com.zp.file.listener.FileInfoListener
+import com.zp.file.listener.FileZipListener
+import com.zp.filemanage.custom.MyFileTypeListener
+import com.zp.filemanage.custom.MyJumpListener
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        FileManageHelp.getInstance()
+                .setFileTypeListener(MyFileTypeListener())
+                .setImgeLoad(MyFileImageListener())
+                .setJumpListener(MyJumpListener())
+                .setFileZipListener(FileZipListener())
+                .setFileInfoListener(FileInfoListener())
+                .setMaxLength(9, "最大选取数量：9")
+                .setCanRightTouch(true)
+                .setShowHiddenFile(false)
+                .setFileFilterArray(arrayOf(PNG, JPG, WAV, MP3, AAC, MP4, _3GP, TXT, ZIP))
+                .setSortordByWhat(FileManageHelp.BY_DEFAULT)
+                .setSortord(FileManageHelp.ASC)
+                .isShowLog = true
+    }
+
+}
